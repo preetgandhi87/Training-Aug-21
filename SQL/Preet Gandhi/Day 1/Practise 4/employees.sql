c95990b201c2ebde3f3227937e420ced074e24de
preetgandhi87 <79098702+preetgandhi87@users.noreply.github.com>
2021-08-27 14:53:01 +0530

    Create employees.sql

--- SQL/Preet Gandhi/Day 1/Practise 4/employees.sql
+++ SQL/Preet Gandhi/Day 1/Practise 4/employees.sql
@@ -1,16 +1,16 @@
-USE rxweb_preetgandhi;
+USE Day1_SQL
 
-CREATE TABLE employees 
+CREATE TABLE employees
 (
-	Employee_Id int PRIMARY KEY IDENTITY(1,1),
-	FirstName varchar(10) NOT NULL,
-	LastName varchar(10) NOT NULL,
-	Email varchar(20) NOT NULL,
-	PhoneNumber numeric(10) DEFAULT NULL,
-	Hire_Date date NOT NULL,
-	Job_Id int NOT NULL FOREIGN KEY REFERENCES jobs(JobId),
-	Salary numeric(6) NOT NULL,
-	Commission numeric(4) DEFAULT NULL,
+	Employee_Id INT PRIMARY KEY IDENTITY(1,1),
+	FirstName VARCHAR(10) NOT NULL,
+	LastName VARCHAR(10) NOT NULL,
+	Email VARCHAR(20) NOT NULL,
+	PhoneNumber NUMERIC(10) DEFAULT NULL,
+	Hire_Date DATE NOT NULL,
+	Job_Id INT NOT NULL FOREIGN KEY REFERENCES jobs(JobId),
+	Salary NUMERIC(6) NOT NULL,
+	Commission NUMERIC(4) DEFAULT NULL,
 	Manager_Id int DEFAULT NULL ,
-	Department_Id int NOT NULL FOREIGN KEY REFERENCES department(department_id),
-);
+	Department_Id INT NOT NULL FOREIGN KEY REFERENCES department(department_id),
+)
