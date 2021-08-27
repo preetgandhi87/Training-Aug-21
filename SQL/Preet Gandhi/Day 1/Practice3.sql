64e48935593e33a525b4f543580bf3e96d742d11
preetgandhi87 <79098702+preetgandhi87@users.noreply.github.com>
2021-08-27 14:50:58 +0530

    Create Practice3.sql

--- SQL/Preet Gandhi/Day 1/Practice3.sql
+++ SQL/Preet Gandhi/Day 1/Practice3.sql
@@ -1,14 +1,14 @@
-USE rxweb_preetgandhi;
+USE Day1_SQL
 
 CREATE TABLE jobs 
 (
-	JobId int PRIMARY KEY IDENTITY(1,1),
-	JobTitle varchar(20) NOT NULL DEFAULT '',
+	JobId INT PRIMARY KEY IDENTITY(1,1),
+	JobTitle VARCHAR(20) NOT NULL DEFAULT '',
 	MinSalary DECIMAL DEFAULT 8000,
 	MaxSalary DECIMAL DEFAULT NULL
-);
+)
 
-INSERT INTO jobs (JobTitle) VALUES ('Developer');
-INSERT INTO jobs (MinSalaey) VALUES (25000);
+INSERT INTO jobs (JobTitle) VALUES ('Developer')
+INSERT INTO jobs (MinSalary) VALUES (25000)
 
-SELECT * FROM jobs;
+SELECT * FROM jobs
