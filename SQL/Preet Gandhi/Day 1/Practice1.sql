cb09bacf866bcc0995624016d52c40593db3c363
preetgandhi87 <79098702+preetgandhi87@users.noreply.github.com>
2021-08-27 14:48:46 +0530

    Create Practice1.sql

--- SQL/Preet Gandhi/Day 1/Practice1.sql
+++ SQL/Preet Gandhi/Day 1/Practice1.sql
@@ -1,14 +1,11 @@
-USE rxweb_preetgandhi;
-
-
 CREATE TABLE countries 
 (
-	CountryId int PRIMARY KEY IDENTITY(1,1),
-	CountryName varchar(20) NOT NULL CONSTRAINT coun_name CHECK (CountryName IN ('Italy','India','China')),
-	RegionId int NOT NULL
-);
+	CountryId INT PRIMARY KEY IDENTITY(1,1),
+	CountryName VARCHAR(20) NOT NULL CONSTRAINT coun_name CHECK (CountryName IN ('Italy','India','China')),
+	RegionId INT NOT NULL
+)
 
 INSERT INTO countries VALUES ('India',10);
-INSERT INTO countries VALUES ('Italy',20), ('China',30);
+INSERT INTO countries VALUES ('Italy',20), ('China',30)
 
-SELECT * FROM countries;
+SELECT * FROM countries
