b46f0d11e41b25333d1b0c8f48b64d7046e1ec69
preetgandhi87 <preetgandhi87@gmail.com>
2021-09-09 14:25:16 +0530

    REFS:#822 and Fixes#822  1)Write queries for additional exercises like CREATE,INSERT,UPDATE,ALTER,SELECT,SubQuery,JOIN,DATE,STRING

--- SQL/Preet Gandhi/Additional Exercises/Exr10_Date_Queries.sql
+++ SQL/Preet Gandhi/Additional Exercises/Exr10_Date_Queries.sql
@@ -1,16 +1,18 @@
 USE Additional_Practice
 --Main Database--
 
---1)Write a query to display the first day of the month (in datetime format) three months before the current month. 
+--1)Write a query to display the first day of the month (in datetime format) three months 
+--  before the current month. 
 --Sample current date : 2014-09-03 Expected result : 2014-06-01
-SELECT DATEFROMPARTS(DATEPART(yy, DATEADD(mm, -3, GETDATE())), DATEPART(mm, DATEADD(mm, -3, GETDATE())), 01)
+SELECT DATEFROMPARTS(DATEPART(yy, DATEADD(mm, -3, GETDATE())), 
+DATEPART(mm, DATEADD(mm, -3, GETDATE())), 01) AS 'Format'
 
---2. Write a query to display the last day of the month (in datetime format) three months before the current month. 
-SELECT DATEFROMPARTS(YEAR(GETDATE()),MONTH(GETDATE()),1)
+--2)Write a query to display the last day of the month (in datetime format) three months 
+--  before the current month. 
 SELECT EOMONTH(GETDATE(), -3)
 
 --3)Write a query to get the distinct Mondays from hire_date in employees tables. 
-SELECT EmployeeID,FirstName,LastName,HireDate FROM Employees WHERE DATEPART(WEEKDAY,HireDate)='2'
+SELECT HireDate FROM Employees WHERE DATENAME(weekday,HireDate) = 'Monday'
 
 --4)Write a query to get the first day of the current year. 
 SELECT DATEFROMPARTS(YEAR(GETDATE()), 1, 1)
@@ -19,66 +21,66 @@ SELECT DATEFROMPARTS(YEAR(GETDATE()), 1, 1)
 SELECT DATEFROMPARTS(YEAR(GETDATE()), 12, 31)
 
 --6)Write a query to calculate the age in year. 
-SELECT DATEDIFF(yy,'2000-08-31',GETDATE())
+SELECT DATEDIFF(yy,'2000-07-08',GETDATE()) AS 'Age'
 
 --7)Write a query to get the current date in the following format. 
 --Sample date : 2014-09-04 Output : September 4, 2014
-SELECT FORMAT(GETDATE(), 'hh:mm tt MMM dd, yyyy') 
+SELECT FORMAT(GETDATE(), 'MMMM d, yyyy') AS 'Format' 
 
 --8)Write a query to get the current date in the following format. 
 --Thursday September 2014
-SELECT DATENAME(WEEKDAY,GETDATE())
-SELECT FORMAT (GETDATE(), 'dd MMM  yyyy') 
+SELECT CONCAT(DATENAME(WEEKDAY,GETDATE()),' ',FORMAT (GETDATE(), ' MMMM yyyy')) AS 'Format'
 
 --9)Write a query to extract the year from the current date. 
-SELECT DATENAME(YEAR,GETDATE())
+SELECT DATENAME(yy,GETDATE()) AS 'Year'
 
 --10)Write a query to get the DATE value from a given day (number in N). 
 --Sample days : 730677 Output : 2000-07-11
-DECLARE @date INT=648
-SELECT DATEADD(DAY,@date,'2021-09-11')
+DECLARE @addDate INT = 2
+SELECT DATEADD(DAY,@addDate,GETDATE())
 
 --11)Write a query to get the first name and hire date from employees table where hire date between '1987-06-01' and '1987-07-30' 
-SELECT FirstName,HireDate FROM Employees WHERE HireDate between '1987-06-01' and '1987-07-30'
-
+SELECT FirstName,HireDate FROM Employees WHERE HireDate BETWEEN '1987-06-01' AND '1987-07-30'
 
 --12)Write a query to display the current date in the following format. 
 --Sample output : Thursday 4th September 2014
-SELECT CONCAT( DATENAME( WEEKDAY, GETDATE()),SPACE(1),DAY( GETDATE()),SPACE(1),DATENAME ( MONTH, GETDATE() ),SPACE(1), YEAR ( GETDATE() ) )
+DECLARE @DateFormat DATETIME
+SET @DateFormat = GETDATE()
+SELECT   CASE
+            WHEN DAY(@DateFormat) IN (1,21,31) THEN (FORMAT(@DateFormat,'dddd d')+'st '+FORMAT(@DateFormat,'MMMM yyyy'))
+            WHEN DAY(@DateFormat) IN (2,22) THEN (FORMAT(@DateFormat,'dddd d')+'nd '+FORMAT(@DateFormat,'MMMM yyyy'))
+            WHEN DAY(@DateFormat) IN (3,23) THEN (FORMAT(@DateFormat,'dddd d')+'rd '+FORMAT(@DateFormat,'MMMM yyyy'))
+            ELSE (FORMAT(@DateFormat,'dddd d')+'th '+FORMAT(@DateFormat,'MMMM yyyy'))
+            END 
 
 --13)Write a query to display the current date in the following format. 
 --Sample output : 05/09/2014
-SELECT CONVERT(VARCHAR, GETDATE(), 3)
+SELECT FORMAT (GETDATE(),'dd/MM/yyyy')
 
 --14)Write a query to display the current date in the following format. 
 --Sample output : 12:00 AM Sep 5, 2014
-SELECT FORMAT (GETDATE(), 'hh:mm tt MMM dd, yyyy') AS TIME
+SELECT FORMAT (GETDATE(), 'hh:mm tt MMM d, yyyy') AS 'Format'
 
 --15)Write a query to get the firstname, lastname who joined in the month of June. 
 SELECT FirstName,LastName FROM Employees WHERE DATENAME(MONTH,HireDate)='June'
 
 --16)Write a query to get the years in which more than 10 employees joined. 
-SELECT DATENAME(YEAR,HireDate) AS 'Years' 
-	FROM Employees 
-	GROUP BY DATENAME(YEAR,HireDate) 
-	HAVING COUNT(EmployeeID) > 10
+SELECT DATENAME(YEAR,HireDate) AS 'Years' FROM Employees 
+GROUP BY DATENAME(YEAR,HireDate) HAVING COUNT(EmployeeID) > 10
 
 --17)Write a query to get the department ID, year, and number of employees joined. 
-SELECT DepartmentID, DATENAME(YEAR,HireDate) AS YEAR, COUNT(EmployeeID) 'NoOfEmployees' 
-	FROM Employees 
-	GROUP BY DepartmentID,DATENAME(YEAR,HireDate) 
-	ORDER BY DepartmentID
+SELECT DepartmentID, DATENAME(YEAR,HireDate) AS 'YEAR', COUNT(EmployeeID) 'NoOfEmployees' FROM Employees 
+GROUP BY DepartmentID,DATENAME(YEAR,HireDate) ORDER BY DepartmentID
 
 --18)Write a query to get department name, manager name, and salary of the manager for all managers whose experience is more than 5 years. 
-SELECT DepartmentName, FirstName, Salary FROM Departments D JOIN Employees E ON (D.ManagerID=E.EmployeeID) 
-	WHERE DATEDIFF(dd,HireDate,GETDATE())/365 > 5
+SELECT DepartmentName, FirstName, Salary FROM Departments D INNER JOIN Employees E ON (D.ManagerID=E.EmployeeID) 
+WHERE DATEDIFF(dd,HireDate,GETDATE())/365 > 5
 
 --19)Write a query to get employee ID, last name, and date of first salary of the employees. 
-SELECT EmployeeID, LastName, EOMONTH(HireDate, 0) AS 'Salary Date' FROM Employees;
+SELECT EmployeeID, LastName, EOMONTH(HireDate, 0) AS 'Salary Date' FROM Employees
 
 --20) Write a query to get first name, hire date and experience of the employees. 
-SELECT FirstName AS 'Name', HireDate,CONCAT(DATEDIFF(dd,HireDate,GETDATE())/365,' ', 'Year',' ',DATEDIFF(MM,HireDate,GETDATE())%12,' ','Month') AS 'Experience' 
-	FROM Employees
+SELECT FirstName,HireDate,DATEDIFF(yy,HireDate,GETDATE()) AS 'Experience' FROM Employees
 
 --21)Write a query to get first name of employees who joined in 1987. 
 SELECT FirstName FROM Employees WHERE DATEPART(yy,HireDate)='1987'
