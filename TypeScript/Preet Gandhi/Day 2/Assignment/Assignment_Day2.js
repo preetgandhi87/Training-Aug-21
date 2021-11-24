fcfc7f5992a4dfdd16ab954c7b2c90af238a5e44
preetgandhi87 <preetgandhi87@gmail.com>
2021-11-24 12:48:57 +0530

    REFS:#2246 and Fixes:#2246 Typescript Day 2: 1)Learn class, Enum, Tuples, interface, Union 2)Practice this concepts 3) Do assignment

---
+++ TypeScript/Preet Gandhi/Day 2/Assignment/Assignment_Day2.js
@@ -0,0 +1,36 @@
+// There is a retail shop which need to manage the inventory, whenever some purchase is being made product quantity should be reduced, if quanity is less than 5 reorder request should be raised.
+// Design an interface and classes for the same.
+var Products = /** @class */ (function () {
+    function Products(id, name, price, quantity) {
+        this.ProductId = id;
+        this.ProductName = name;
+        this.ProductPrice = price;
+        this.ProductQuantity = quantity;
+    }
+    return Products;
+}());
+var obj = [];
+
+function addProducts(ProductId, ProductName, ProductPrice, ProductQuantity) {
+    obj.push({ ProductId: ProductId, ProductName: ProductName, ProductPrice: ProductPrice, ProductQuantity: ProductQuantity });
+}
+addProducts(1, "Colgate", 100, 20);
+addProducts(2, "Close UP", 90, 15);
+addProducts(3, "Dabar Red", 110, 25);
+function Purchase(Product_Name, Product_Quantity) {
+    obj.filter(function (value, index, arr) {
+        if (value.ProductQuantity > 5) {
+            if (value.ProductName == Product_Name) {
+                value.ProductQuantity = value.ProductQuantity - Product_Quantity;
+            }
+            else {
+                console.log("Please enter valid name");
+            }
+        }
+        else {
+            console.log("Please enter quantity more than 5");
+        }
+    });
+}
+Purchase("Colgate", 9);
+console.log(obj);
